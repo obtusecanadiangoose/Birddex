857bf296449523d932471170a09d9fc333f3ac94
Liam <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>
2021-09-30 13:03:31 +0000

    trying to mesh JS and Python

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import plotly.graph_objects as go
 import dash
 from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
+#import dash_core_components as dcc
+from dash import dcc
+#import dash_html_components as html
+from dash import html
 import sd_material_ui
 
 from geopy.geocoders import Nominatim
--- supportingActors/main_clickable.py
+++ supportingActors/main_clickable.py
@@ -1,8 +1,10 @@
 import plotly.graph_objects as go
 import dash
 from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
+#import dash_core_components as dcc
+from dash import dcc
+#import dash_html_components as html
+from dash import html
 import sd_material_ui
 
 from geopy.geocoders import Nominatim
@@ -12,17 +14,32 @@ geolocator = Nominatim(user_agent='myapplication')
 mapbox_access_token = open(".mapbox_token").read()
 
 fig = go.Figure(go.Scattermapbox(
-    lat=['38.91427'],
-    lon=['-77.02827'],
-    mode='markers',
-    marker=go.scattermapbox.Marker(
-        size=9
-    ),
-    text=["The coffee bar"],
 
-))
+        lat=['38.91427','38.91538','38.91458',
+             '38.92239','38.93222','38.90842',
+             '38.91931','38.93260','38.91368',
+             '38.88516','38.921894','38.93206',
+             '38.91275'],
+        lon=['-77.02827','-77.02013','-77.03155',
+             '-77.04227','-77.02854','-77.02419',
+             '-77.02518','-77.03304','-77.04509',
+             '-76.99656','-77.042438','-77.02821',
+             '-77.01239'],
+        mode="markers",
+        marker=go.scattermapbox.Marker(
+            size=9
+        ),
+        text=["The coffee bar","Bistro Bohem","Black Cat",
+             "Snap","Columbia Heights Coffee","Azi's Cafe",
+             "Blind Dog Cafe","Le Caprice","Filter",
+             "Peregrine","Tryst","The Coupe",
+             "Big Bear Cafe"],
+    ))
+
+fig.update_layout(clickmode='event+select')
 
 fig.update_layout(
+    #showlegend=False,
     margin={
         'l': 0,
         'r': 0,
@@ -47,13 +64,15 @@ fig.update_layout(
         zoom=10
     ),
 )
-
+app = dash.Dash(__name__)
+'''
 app = dash.Dash(
     '',
     external_stylesheets=[
         'https://fonts.googleapis.com/icon?family=Material+Icons',
     ]
 )
+'''
 
 app.layout = html.Div([
     html.Div([
@@ -67,16 +86,9 @@ app.layout = html.Div([
         dcc.Graph(
             id='map',
             figure=fig,
-            style={'width': '80vw', 'height': '90vh', 'float': 'left', 'margin': 'auto'}
+            style={'width': '80vw', 'height': '90vh', 'float': 'left', 'margin': 'auto'},
+            #clickData="string?"
         ),
-        html.Pre(
-            id='structure',
-            style={
-                'border': 'thin lightgrey solid',
-                'overflowY': 'scroll',
-                'height': '275px'
-        }
-    ),
 
     ]),
     html.Div([
@@ -103,59 +115,12 @@ app.layout = html.Div([
     ])
 ])
 
-"""
 @app.callback(
-    Output("map", "figure"),
-    Input("button1", "n_clicks"),
-    Input('button2', 'n_clicks'),
-    [dash.dependencies.State('input1', 'value')]
-)
-def change_map_centre(btn1, btn2, value):
-    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-    print(changed_id)
-    if changed_id == "button1.n_clicks":
-        print("Button 1 Clicked")
-        location = geolocator.geocode(value)
-        fig.update_layout(
-            margin={
-                'l': 0,
-                'r': 0,
-                'b': 0,
-                't': 0,
-                'pad': 0,
-            },
-            autosize=True,
-            hovermode='closest',
-            mapbox=dict(
-                # dragRotate=False,
-                style='mapbox://styles/mapbox/outdoors-v11',
-                accesstoken=mapbox_access_token,
-                bearing=0,
-                center=dict(
-                    # lat=38.92,
-                    lat=location.latitude,
-                    # lon=-77.07
-                    lon=location.longitude
-                ),
-                pitch=0,
-                zoom=10
-            ),
-        )
-        return fig
-
-    elif changed_id == "button2.n_clicks":
-        print("Button 2 Clicked")
-        return fig
-    else:
-        print("nothing clicked yet...")
-        return fig
-        
-"""
-@app.callback(
-    Output("structure", "children"),
-    [Input("graph", "figure")])
-def display_structure(fig_json):
-    return 0#json.dumps(fig_json, indent=2)
+    Output('map', 'figure'),
+    Input('map', 'clickData'))
+def display_click_data(clickData):
+    print("clicked")
+    return fig
 
 
 app.run_server(debug=True, use_reloader=True)  # Turn off reloader if inside Jupyter
